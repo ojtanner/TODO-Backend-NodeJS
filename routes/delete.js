ca7cb6981c7da1946fdc6c9f802463a734e7fbd4
ojtanner <oliver-tanner@outlook.com>
2019-08-01 18:01:57 +0200

    Finish basic api

--- routes/delete.js
+++ routes/delete.js
@@ -1,7 +1,22 @@
 const deleteRouter = require('express').Router();
+const todos = require('../model/index');
 
 deleteRouter.delete('/', (req, resp) => {
-    resp.send('Your deleted todo');
+    const todoId = req.body.id;
+
+    if(!todoId) {
+        resp.status(404).send({ id: -1 });
+        return;
+    }
+
+    const todo = todos.remove(todoId);
+
+    if(todo.id === -1) {
+        resp.status(404).send(todo);
+        return;
+    }
+
+    resp.status(200).send(todo);
 });
 
 module.exports = deleteRouter;
--- routes/patch.js
+++ routes/patch.js
@@ -1,7 +1,23 @@
 const patchRouter = require('express').Router();
+const todos = require('../model/index');
 
 patchRouter.patch('/', (req, resp) => {
-    resp.send('Your patched todo');
+    const todoId = req.body.id;
+
+    if(!todoId) {
+        resp.status(404).send({ id: -1 });
+        return;
+    }
+
+    const updatedTodoProperties = req.body.update;
+
+    if(!updatedTodoProperties) {
+        resp.status(404).send({ id: -1 });
+        return;
+    }
+
+    const updatedTodo = todos.update(todoId, updatedTodoProperties);
+    resp.status(200).send(updatedTodo);
 });
 
 module.exports = patchRouter;
