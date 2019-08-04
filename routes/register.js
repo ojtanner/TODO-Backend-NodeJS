eb669188af52788a241ae19cc70e702bca0cd541
ojtanner <oliver-tanner@outlook.com>
2019-08-04 21:25:39 +0200

    Check for email in use

--- routes/register.js
+++ routes/register.js
@@ -2,12 +2,20 @@ const registerRouter = require('express').Router();
 const { User } = require('../model/database/mongoose');
 
 registerRouter.post('/', async (req, resp) => {
+
+    try {
+        const emailInUse = await User.findOne({ email: req.body.email });
+        if(emailInUse) return resp.status(401).send('Email in use');
+    } catch (error) {
+        return resp.status(400).send(error);
+    }
+
     const user = new User(req.body);
     try {
         await user.save();
-        resp.status(200).send(user);
+        return resp.status(200).send(user);
     } catch (error) {
-        resp.status(401).send(error);
+        return resp.status(401).send(error);
     }
 });
 
