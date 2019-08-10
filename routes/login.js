35ed719cab059d0af20924c28366c1816073c789
ojtanner <oliver-tanner@outlook.com>
2019-08-10 09:56:21 +0200

    Add jwt to login

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 const loginRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const { User } = require('../model/database/mongoose');
 
 loginRouter.post('/', async (req, resp) => {
@@ -8,7 +9,8 @@ loginRouter.post('/', async (req, resp) => {
         if(!user) return resp.status(400).send('Invalid email');
         const passwordIsValid = await user.validatePassword(req.body.password);
         if(!passwordIsValid) resp.status(400).send('Invalid password');
-        return resp.status(200).send('Login successful');
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_OR_KEY);
+        return resp.status(200).send(token);
     } catch (error) {
         return resp.status(500).send(error);
     }
