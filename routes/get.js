7974e032cd74c2555cc679e42950d8949b41e006
ojtanner <oliver-tanner@outlook.com>
2019-08-10 09:56:50 +0200

    Use jwt for user specific Todo retrieval

--- routes/get.js
+++ routes/get.js
@@ -1,7 +1,21 @@
 const getRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
+const ObjectId = require('mongoose').Types.ObjectId;
+const { Todo } = require('../model/database/mongoose');
 
-getRouter.get('/', (req, res) => {
-
+getRouter.get('/', async (req, res) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) res.send(400).send('No auth header');
+    const [authType, authToken] = authHeader.split(" ");
+    if(!authType || authType !== 'Bearer' || !authToken) return res.status(400).send('Invalid auth header');
+    const decodedToken = jwt.verify(authToken, process.env.SECRET_OR_KEY);
+    
+    try {
+        const todos = await Todo.find({ owner: new ObjectId(decodedToken.id) });
+        return res.status(200).send(todos);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 });
 
 module.exports = getRouter;
