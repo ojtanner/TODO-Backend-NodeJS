19f3755b34dd3ebae25656cc0a0f714e81f19e9e
ojtanner <oliver-tanner@outlook.com>
2019-08-21 14:47:53 +0200

    Configure Mongoose
    Configures mongoose to use the query findOneAndUpdate from mongodb

--- model/database/mongoose.js
+++ model/database/mongoose.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const User = require('./user.model');
 const Todo = require('./todo.model');
 
+mongoose.set('useFindAndModify', false);
+
 mongoose.connect(process.env.MONGODB_URL,
     { useNewUrlParser: true, useCreateIndex: true },
     () => console.log('connected'));
