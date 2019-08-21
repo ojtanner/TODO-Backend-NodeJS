fe9273ce5319b40968251a62feb0098d44cee10b
ojtanner <oliver-tanner@outlook.com>
2019-08-21 15:02:41 +0200

    Configure Delete Route

--- routes/delete.js
+++ routes/delete.js
@@ -1,7 +1,18 @@
 const deleteRouter = require('express').Router();
+const ObjectId = require('mongoose').Types.ObjectId;
+const { Todo } = require('../model/database/mongoose');
+const verifyAuthentification = require('../middleware/verifyAuthentication');
 
-deleteRouter.delete('/', (req, resp) => {
+deleteRouter.delete('/', verifyAuthentification, async (req, resp) => {
+    try {
+        const deletedTodo = await Todo.findOneAndDelete({owner: new ObjectId(req.token.id), _id: new ObjectId(req.body.id)});
 
+        if(!deletedTodo) return resp.status(400).send('Invalid delete id');
+
+        return resp.status(200).send(deletedTodo);
+    } catch (error) {
+        return resp.status(500).send('Internal error');
+    }
 });
 
 module.exports = deleteRouter;
