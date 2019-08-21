b0e7a9ac3ea236eb8a4c7d9701e246fc3c1b5ae0
ojtanner <oliver-tanner@outlook.com>
2019-08-21 14:48:06 +0200

    Set Up Patch Route

--- routes/patch.js
+++ routes/patch.js
@@ -1,7 +1,20 @@
 const patchRouter = require('express').Router();
+const ObjectId = require('mongoose').Types.ObjectId;
+const { Todo } = require('../model/database/mongoose');
+const verifyAuthentification = require('../middleware/verifyAuthentication');
 
-patchRouter.patch('/', (req, resp) => {
-
+patchRouter.patch('/', verifyAuthentification, async (req, resp) => {
+    try {
+        const todo = await Todo.findOneAndUpdate(
+            { owner: new ObjectId(req.token.id), _id: new ObjectId(req.body.id)},
+            {...req.body},
+            { new: true}
+            );
+        return resp.status(200).send(todo);
+    } catch (error) {
+        console.log(error);
+        return resp.status(400).send('Invalid Update');
+    }
 });
 
 module.exports = patchRouter;
