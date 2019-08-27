5279167682bdf95ccd608fe31869bbd03b00c4ac
ojtanner <oliver-tanner@outlook.com>
2019-08-27 20:01:39 +0200

    Set Up Initial ProblemDetails

--- utils/ProblemDetails.js
+++ utils/ProblemDetails.js
@@ -4,19 +4,29 @@ const problemTypeEnum = {
         TYPE: '/problems/invalid_credentials',
         TITLE: 'Invalid Credentials',
         STATUS: 400
+    },
+    INTERNAL_ERROR: {
+        KEY: 'internal_error',
+        TYPE: '/problems/internal_error',
+        TITLE:'Internal Error',
+        STATUS: 500
     }
 }
 
 const ProblemDetails = function ( problemType ) {
     switch (problemType) {
         case problemTypeEnum.INVALID_CREDENTIALS.KEY:
-            
             this.type = problemTypeEnum.INVALID_CREDENTIALS.TYPE,
             this.title = problemTypeEnum.INVALID_CREDENTIALS.TITLE,
             this.status = problemTypeEnum.INVALID_CREDENTIALS.STATUS
-
             break;
     
+        case problemTypeEnum.INTERNAL_ERROR.KEY:
+            this.type = problemTypeEnum.INTERNAL_ERROR.TYPE,
+            this.title = problemTypeEnum.INTERNAL_ERROR.TITLE,
+            this.status = problemTypeEnum.INTERNAL_ERROR.STATUS
+            break;
+
         default:
             break;
     }
