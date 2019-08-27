5c787d830f14a4a0f0e0921bc5636cdd9b047542
ojtanner <oliver-tanner@outlook.com>
2019-08-27 20:01:52 +0200

    Implement ProbDetails For /login

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,35 @@
 const loginRouter = require('express').Router();
 const jwt = require('jsonwebtoken');
+const { User } = require('../mongoose/mongoose');
+const ProblemDetails = require('../utils/ProblemDetails');
 
 loginRouter.post('/', async (req, resp) => {
 
     try {
         const user = await User.findOne({ email: req.body.email });
-        if(!user) return resp.status(400).send('Invalid email');
+        if(!user) {
+            const problem = new ProblemDetails('invalid_credentials');
+            return resp.status(problem.status)
+                .contentType('application/problem+json')
+                .send(problem);
+        }
+
         const passwordIsValid = await user.validatePassword(req.body.password);
-        if(!passwordIsValid) resp.status(400).send('Invalid password');
+        if(!passwordIsValid) {
+            const problem = new ProblemDetails('invalid_credentials');
+            return resp.status(problem.status)
+                .contentType('application/problem+json')
+                .send(problem);
+        }
+
         const token = jwt.sign({ id: user._id }, process.env.SECRET_OR_KEY);
         return resp.status(200).send(token);
     } catch (error) {
-        return resp.status(500).send(error);
+        console.log(error)
+        const problem = new ProblemDetails('internal_error');
+        return resp.status(problem.status)
+            .contentType('application/problem+json')
+            .send(problem);
     }
 });
 
